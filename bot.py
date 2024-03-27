b30d246578cfe8ee1e3d3e90a59ef55d83d8d1f5
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-03-26 22:56:10 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,72 @@
+#(©)Codexbotz
+
+from aiohttp import web
+from plugins import web_server
+
+import pyromod.listen
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
+
+from config import API_HASH, ultroidxTeam_api_id, LOGGER, ultroidxTeam_bot_token, ultroidxTeam_botWorkers, FORCE_SUB_CHANNEL, ultroidxTeam_logChannel_id, PORT
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=ultroidxTeam_api_id,
+            plugins={
+                "root": "plugins"
+            },
+            workers=ultroidxTeam_botWorkers,
+            bot_token=ultroidxTeam_bot_token
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+                sys.exit()
+        try:
+            db_channel = await self.get_chat(ultroidxTeam_logChannel_id)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the ultroidxTeam_logChannel_id Value, Current Value {ultroidxTeam_logChannel_id}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+            sys.exit()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f""" \n\n       
+(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+░╚════╝░░╚════╝░╚═════╝░╚══════╝
+                                          """)
+        self.username = usr_bot_me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
---
+++ config.py
@@ -0,0 +1,88 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+ultroidxTeam_bot_token = os.environ.get("ultroidxTeam_bot_token", "6976814117:AAEFE6XS95sfM085--KzXrFZ420Ee_VTLNA")
+
+#Your API ID from my.telegram.org
+ultroidxTeam_api_id = int(os.environ.get("ultroidxTeam_api_id", "22505271"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "c89a94fcfda4bc06524d0903977fc81e")
+
+#Your db channel Id
+ultroidxTeam_logChannel_id = int(os.environ.get("ultroidxTeam_logChannel_id", "-1002075726565"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6695586027"))
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+ultroidxTeam_DB_URI = "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"
+ultroidxTeam_DB_name = os.environ.get("DATABASE_NAME", "ultroidxTeam")
+
+ultroidxTeam_short_URL = os.environ.get("ultroidxTeam_short_URL", "api.shareus.io")
+ultroidxTeam_short_API = os.environ.get("ultroidxTeam_short_API", "PUIAQBIFrydvLhIzAOeGV8yZppu2")
+ultroidxTeam_Timeout = int(os.environ.get('ultroidxTeam_Timeout', 86400)) # Add time in seconds
+ultroidxTeam_IS_VERIFY = os.environ.get("ultroidxTeam_IS_VERIFY", "True")
+ultroidxTeam_tutorial = os.environ.get("ultroidxTeam_tutorial","gojfsi/2")
+
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001982072622"))
+
+ultroidxTeam_botWorkers = int(os.environ.get("ultroidxTeam_botWorkers", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+try:
+    ultroidxTeam_ADMINS=[]
+    for x in (os.environ.get("ultroidxTeam_ADMINS", "6852649461").split()):
+        ultroidxTeam_ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your ultroidxTeam_ADMINS list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+ultroidxTeam_botSTATS = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+
+ultroidxTeam_ADMINS.append(OWNER_ID)
+ultroidxTeam_ADMINS.append(6852649461)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
