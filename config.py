4a61bcb762b92c27e1f771388080a9c8aad6bc42
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-10 22:31:01 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,18 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
 #Bot token @Botfather
-ultroidxTeam_bot_token = os.environ.get("ultroidxTeam_bot_token", "6976814117:AAEFE6XS95sfM085--KzXrFZ420Ee_VTLNA")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-#Your API ID from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
-ultroidxTeam_api_id = int(os.environ.get("ultroidxTeam_api_id", "22505271"))
+#Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
 
-#Your API Hash from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
+#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
-#Your db channel Id 
-ultroidxTeam_logChannel_id = int(os.environ.get("ultroidxTeam_logChannel_id", "-1002075726565"))
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
@@ -22,36 +21,34 @@ OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 #Port
 PORT = os.environ.get("PORT", "8080")
 
+#Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-
-ultroidxTeam_DB_URI = "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"
-ultroidxTeam_DB_name = os.environ.get("DATABASE_NAME", "ultroidxTeam")
+DB_URI = "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
 # 
 
-ultroidxTeam_short_URL = os.environ.get("ultroidxTeam_short_URL", "api.shareus.io")
-ultroidxTeam_short_API = os.environ.get("ultroidxTeam_short_API", "PUIAQBIFrydvLhIzAOeGV8yZppu2")
-ultroidxTeam_Timeout = int(os.environ.get('ultroidxTeam_Timeout', 86400)) # Add time in seconds
-ultroidxTeam_IS_VERIFY = os.environ.get("ultroidxTeam_IS_VERIFY", "True")
-ultroidxTeam_tutorial = os.environ.get("ultroidxTeam_tutorial","gojfsi/2")
-
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.shareus.io")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "PUIAQBIFrydvLhIzAOeGV8yZppu2")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001982072622"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 
-# no need to chnage 
-ultroidxTeam_botWorkers = int(os.environ.get("ultroidxTeam_botWorkers", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ultroidxTeam_ADMINS=[]
-    for x in (os.environ.get("ultroidxTeam_ADMINS", "6852649461").split()):
-        ultroidxTeam_ADMINS.append(int(x))
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6852649461").split()):
+        ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your ultroidxTeam_ADMINS list does not contain valid integers.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
@@ -65,11 +62,11 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-ultroidxTeam_botSTATS = "<b>BOT UPTIME</b>\n{uptime}"
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
-ultroidxTeam_ADMINS.append(OWNER_ID)
-ultroidxTeam_ADMINS.append(6852649461)
+ADMINS.append(OWNER_ID)
+ADMINS.append(6852xxxx)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -91,4 +88,3 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-
