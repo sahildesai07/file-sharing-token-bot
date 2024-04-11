7f681b9f37bfea74cc16eb2d787516594e3cf7a1
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-10 22:54:43 -0700

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,16 +1,17 @@
+
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ultroidxTeam_ADMINS, ultroidxTeam_botSTATS, USER_REPLY_TEXT
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
-@Bot.on_message(filters.command('stats') & filters.user(ultroidxTeam_ADMINS))
+@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
-    await message.reply(ultroidxTeam_botSTATS.format(uptime=time))
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
 @Bot.on_message(filters.private & filters.incoming)
