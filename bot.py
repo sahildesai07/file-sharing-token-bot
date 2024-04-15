676384ed8d233f5873fa9ebb242bf8f053b05d63
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-15 02:49:11 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,6 @@ from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
-# modified by @ultroidxTeam (TG)
-
 class Bot(Client):
     def __init__(self):
         super().__init__(
@@ -27,59 +25,47 @@ class Bot(Client):
         self.LOGGER = LOGGER
 
     async def start(self):
-        try:
-            await super().start()
-            usr_bot_me = await self.get_me()
-            self.uptime = datetime.now()
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
 
-            for i in range(1, 5):
-                force_sub_channel = globals().get(f"FORCE_SUB_CHANNEL_{i}")
-                if force_sub_channel:
-                    await self.handle_force_sub_channel(i, force_sub_channel)
-
-            await self.test_db_channel()
-
-            self.set_parse_mode(ParseMode.HTML)
-            self.LOGGER(__name__).info("Bot Running..!")
-            self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
-░╚════╝░░╚════╝░╚═════╝░╚════╝░
-                                          """)
-            # Start web server
-            await self.start_web_server()
-
-        except Exception as e:
-            self.LOGGER(__name__).error(f"Error during bot startup , need help ? t.me/ultroidofficial_chat: {e}")
-            sys.exit(1)
-
-    async def handle_force_sub_channel(self, idx, force_sub_channel):
-        try:
-            link = (await self.get_chat(force_sub_channel)).invite_link
-            if not link:
-                await self.export_chat_invite_link(force_sub_channel)
-                link = (await self.get_chat(force_sub_channel)).invite_link
-            setattr(self, f"invitelink{idx}", link)
-        except Exception as e:
-            self.LOGGER(__name__).warning(f"Error handling FORCE_SUB_CHANNEL_{idx} , need help ? t.me/ultroidofficial_chat : {e}")
-
-    async def test_db_channel(self):
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+                sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
-            test_msg = await self.send_message(chat_id=db_channel.id, text="Test Message")
-            await test_msg.delete()
             self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(f"Error testing DB channel , need help ? t.me/ultroidofficial_chat: {e}")
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroid_official for support")
+            sys.exit()
 
-    async def start_web_server(self):
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f""" \n\n       
+(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+░╚════╝░░╚════╝░╚═════╝░╚══════╝
+                                          """)
+        self.username = usr_bot_me.username
+        #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
-        try:
-            await super().stop()
-            self.LOGGER(__name__).info("Bot stopped.")
-        except Exception as e:
-            self.LOGGER(__name__).error(f"Error while stopping bot , need help ? t.me/ultroidofficial_chat : {e}")
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
