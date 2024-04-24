aa1e26e6d424ac05f9bd74eb0c0ab35c351a7579
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-24 04:40:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = ""
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
 
 #Shortner (token system) 
