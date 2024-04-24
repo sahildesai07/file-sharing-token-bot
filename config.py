88f87667df38ad5b2c6f03edd29641e07994c47d
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-24 04:38:35 -0700

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.shareus.io")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "PUIAQBIFrydvLhIzAOeGV8yZppu2")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/Ultroid_Official/18")
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/Ultroid_Official/424")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
