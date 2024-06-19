0e83571d3b6f94b6b1a68d7e535a54f8bfafe90a
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-19 08:52:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "b323441bc55ef14be63018992134d393a62a9da9")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/Ultroid_Official/18") # shareus ka tut_vid he 
 
