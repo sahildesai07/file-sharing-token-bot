29d7198913557b5c2d2e8994ef443ca2be0df582
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-07-21 16:59:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 from aiohttp import web
 from plugins import web_server
 
-import pyrofork.listen
+import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
