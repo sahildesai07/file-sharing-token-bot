0e094ed9d76103c2ec29c58d148d69a65e96fd6d
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-24 13:27:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002075726565"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002043373014"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
