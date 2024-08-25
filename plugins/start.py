f935b8feaf1219e06202ffe9d9efa4d735ef08d4
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-25 11:32:20 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,7 +18,7 @@ REQ_JOIN_LINK2 = 'https://t.me/+CtzZboehkKBmNmFk'
 # MongoDB setup
 MONGO_URL = 'mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority'
 mongo_client = MongoClient(MONGO_URL)
-db = mongo_client['fwggih']  # Replace with your database name
+db = mongo_client['fwih']  # Replace with your database name
 collection = db['join_requests']  # Replace with your collection name
 
 #Bot = Client("my_bot")
