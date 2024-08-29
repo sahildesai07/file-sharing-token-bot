108c3a15313e5e9464ee913057520c826a98237d
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-29 10:51:19 -0700

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,54 +1,59 @@
-
+import motor.motor_asyncio
+import time
 from config import DB_URI, DB_NAME
-from pymongo import MongoClient
 
-client = MongoClient(DB_URI)
+#MONGO_DB_URL = 'your_mongodb_connection_string_here'
+client = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 db = client[DB_NAME]
-users = db["users"]
+users_collection = db['users']
 
-async def add_user(id):
-    users.insert_one({"_id": id, "limit": 10, "is_verified": False, "verify_token": "", "verified_time": 0})
+async def add_user(user_id):
+    user_data = {
+        "_id": user_id,
+        "limits": 10,  # Initial limit for the user
+        "is_verified": False,
+        "verified_time": None,
+        "verify_token": None,
+        "created_at": time.time()
+    }
+    await users_collection.insert_one(user_data)
 
-async def present_user(user_id: int):
-    found = await user_data.find_one({'_id': user_id})
-    return bool(found)
+async def present_user(user_id):
+    user = await users_collection.find_one({"_id": user_id})
+    return bool(user)
 
-async def db_verify_status(user_id):
-    user = await user_data.find_one({'_id': user_id})
+async def get_verify_status(user_id):
+    user = await users_collection.find_one({"_id": user_id})
     if user:
-        return user.get('verify_status', default_verify)
-    return default_verify
+        return {
+            "is_verified": user.get("is_verified", False),
+            "verified_time": user.get("verified_time"),
+            "verify_token": user.get("verify_token"),
+            "limits": user.get("limits", 0)
+        }
+    return None
 
-async def db_update_verify_status(user_id, verify):
-    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
-
-async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = [doc['_id'] async for doc in user_docs]
-    return user_ids
-
-async def del_user(user_id: int):
-    await user_data.delete_one({'_id': user_id})
-    return
-
-async def update_user_limit(id, limit):
-    users.update_one({"_id": id}, {"$set": {"limit": limit}})
-
-async def get_user_limit(id):
-    user = users.find_one({"_id": id})
-    return user.get("limit", 0) if user else 0
-
-async def get_verify_status(id):
-    user = users.find_one({"_id": id})
-    return user if user else {"is_verified": False, "verify_token": "", "verified_time": 0}
-
-async def update_verify_status(id, is_verified=None, verify_token=None, verified_time=None):
-    update_fields = {}
+async def update_verify_status(user_id, is_verified=None, verified_time=None, verify_token=None, limits=None):
+    update_data = {}
     if is_verified is not None:
-        update_fields["is_verified"] = is_verified
-    if verify_token is not None:
-        update_fields["verify_token"] = verify_token
+        update_data["is_verified"] = is_verified
     if verified_time is not None:
-        update_fields["verified_time"] = verified_time
+        update_data["verified_time"] = verified_time
+    if verify_token is not None:
+        update_data["verify_token"] = verify_token
+    if limits is not None:
+        update_data["limits"] = limits
 
-    users.update_one({"_id": id}, {"$set": update_fields})
+    await users_collection.update_one({"_id": user_id}, {"$set": update_data})
+
+async def decrement_user_limit(user_id):
+    await users_collection.update_one({"_id": user_id}, {"$inc": {"limits": -1}})
+
+async def get_user_limit(user_id):
+    user = await users_collection.find_one({"_id": user_id})
+    if user:
+        return user.get("limits", 0)
+    return 0
+
+async def reset_user_limits(user_id, new_limit=10):
+    await users_collection.update_one({"_id": user_id}, {"$set": {"limits": new_limit}})
