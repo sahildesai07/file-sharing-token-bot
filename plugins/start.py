041fe04a753bfa7b4154ceacbaeb601c76a09991
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-30 04:57:39 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -97,6 +97,8 @@ async def start_command(client: Client, message: Message):
                     pass
         return
 
+    else:
+
     reply_markup = InlineKeyboardMarkup(
         [
             [
