b41f78996d0f426d312beec679f8f26e5b4d9df8
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-30 05:04:44 -0700

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,41 +1,12 @@
-import motor.motor_asyncio
-from config import DB_URI, DB_NAME
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import DB_URI, DB_NAME , START_COMMAND_LIMIT
 
-dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
-database = dbclient[DB_NAME]
+mongo_client = AsyncIOMotorClient(DB_URI)
+db = mongo_client[DB_NAME]
 
-user_data = database['users']
-# Define collections
-user_collection = database['user_collection']
-token_collection = database['tokens']
+user_collection = db['user_collection']
+token_collection = db['tokens']
 
-default_verify = {
-    'is_verified': False,
-    'verified_time': 0,
-    'verify_token': "",
-    'link': ""
-}
-
-default_user = {
-    '_id': None,
-    'verify_status': default_verify,
-    'credits': 0  # Default credit amount
-}
-
-
-async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = [doc['_id'] async for doc in user_docs]
-    return user_ids
-
-async def del_user(user_id: int):
-    await user_data.delete_one({'_id': user_id})
-    return
-
-
-
-
-# Functions to manage users and tokens
 async def add_user(user_id):
     await user_collection.insert_one({
         "_id": user_id,
@@ -49,9 +20,7 @@ async def get_user_limit(user_id):
     return 0
 
 async def update_user_limit(user_id, new_limit):
-    result = await user_collection.update_one({"_id": user_id}, {"$set": {"limit": new_limit}})
-    if result.modified_count == 0:
-        logger.info(f"No document updated for user_id: {user_id}")
+    await user_collection.update_one({"_id": user_id}, {"$set": {"limit": new_limit}})
 
 async def store_token(user_id, token):
     await token_collection.insert_one({
@@ -66,4 +35,5 @@ async def verify_token(user_id, token):
         await token_collection.update_one({"_id": token_data['_id']}, {"$set": {"used": True}})
         return True
     return False
+
 
