23630a9d4468a15d70f5d4d6cf714e9cafe4b76e
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-30 00:49:19 -0700

    Update database.py

--- database/database.py
+++ database/database.py
@@ -5,6 +5,9 @@ dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 user_data = database['users']
+# Define collections
+user_collection = db['user_collection']
+token_collection = db['tokens']
 
 default_verify = {
     'is_verified': False,
@@ -19,31 +22,6 @@ default_user = {
     'credits': 0  # Default credit amount
 }
 
-def new_user(id):
-    return {
-        '_id': id,
-        'verify_status': default_verify,
-        'credits': 0  # Default credit amount
-    }
-
-async def present_user(user_id: int):
-    found = await user_data.find_one({'_id': user_id})
-    return bool(found)
-"""
-async def add_user(user_id: int):
-    user = new_user(user_id)
-    await user_data.insert_one(user)
-    return
-"""
-
-async def db_verify_status(user_id):
-    user = await user_data.find_one({'_id': user_id})
-    if user:
-        return user.get('verify_status', default_verify)
-    return default_verify
-
-async def db_update_verify_status(user_id, verify):
-    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
 
 async def full_userbase():
     user_docs = user_data.find()
@@ -54,53 +32,38 @@ async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
     return
 
-# Credit management functions
 
-async def get_credits(user_id: int):
-    user = await user_data.find_one({'_id': user_id})
-    if user:
-        return user.get('credits', 0)
-    return 0
 
-async def update_credits(user_id: int, new_credits: int):
-    await user_data.update_one({'_id': user_id}, {'$set': {'credits': new_credits}})
-    return
 
-async def increment_credits(user_id: int, amount: int):
-    user = await user_data.find_one({'_id': user_id})
-    if user:
-        current_credits = user.get('credits', 0)
-        new_credits = current_credits + amount
-        await update_credits(user_id, new_credits)
-    return
-
-async def decrement_credits(user_id: int, amount: int):
-    user = await user_data.find_one({'_id': user_id})
-    if user:
-        current_credits = user.get('credits', 0)
-        new_credits = max(current_credits - amount, 0)
-        await update_credits(user_id, new_credits)
-    return
-
-from pymongo import MongoClient
-
-# MongoDB setup
-#client = MongoClient("mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority")
-#db = client.bot_database
-#users_collection = db.users
-
+# Functions to manage users and tokens
 async def add_user(user_id):
-    users_collection.update_one({'_id': user_id}, {'$set': {'credits': 0}}, upsert=True)
+    await user_collection.insert_one({
+        "_id": user_id,
+        "limit": START_COMMAND_LIMIT
+    })
 
-async def get_user_credits(user_id):
-    user = users_collection.find_one({'_id': user_id})
-    if user:
-        return user.get('credits', 0)
+async def get_user_limit(user_id):
+    user_data = await user_collection.find_one({"_id": user_id})
+    if user_data:
+        return user_data.get('limit', 0)
     return 0
 
-async def update_user_credits(user_id, amount):
-    users_collection.update_one({'_id': user_id}, {'$inc': {'credits': amount}}, upsert=True)
+async def update_user_limit(user_id, new_limit):
+    result = await user_collection.update_one({"_id": user_id}, {"$set": {"limit": new_limit}})
+    if result.modified_count == 0:
+        logger.info(f"No document updated for user_id: {user_id}")
 
-async def add_user_credits(user_id, amount):
-    await update_user_credits(user_id, amount)
+async def store_token(user_id, token):
+    await token_collection.insert_one({
+        "user_id": user_id,
+        "token": token,
+        "used": False
+    })
+
+async def verify_token(user_id, token):
+    token_data = await token_collection.find_one({"user_id": user_id, "token": token, "used": False})
+    if token_data:
+        await token_collection.update_one({"_id": token_data['_id']}, {"$set": {"used": True}})
+        return True
+    return False
 
