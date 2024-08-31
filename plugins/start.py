fe498d10a7e130154991d98186b6aec64a8e4552
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-31 00:21:52 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,7 +29,7 @@ from config import (
     OWNER_ID,
 )
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
-from database.database import add_user, del_user, full_userbase, present_user , reset_daily_counts , clean_old_verifications , db_verify_status
+from database.database import add_user, del_user, full_userbase, present_user , reset_daily_counts , clean_old_verifications , db_verify_status , user_data
 from shortzy import Shortzy
 
 
