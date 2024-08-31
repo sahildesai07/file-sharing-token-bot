04d472044dff38e145b3d97f12fa6516ed9b40dc
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-31 02:47:42 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -39,7 +39,7 @@ async def get_verification_count(user_id):
     return 0
 
 @Bot.on_message(filters.command('verify_count') & filters.private)
-async def verify_count_command(client: Client, message: Message):
+async def verify_count_command(client: Bot, message: Message):
     user_id = message.from_user.id
     verify_status = await db_verify_status(user_id)
     verify_count = verify_status.get('verify_count', 0)  # Ensure 'verify_count' is managed
@@ -47,7 +47,7 @@ async def verify_count_command(client: Client, message: Message):
 
 
 @Bot.on_message(filters.command('verify_stats') & filters.private & filters.user(ADMINS))
-async def verify_stats_command(client: Client, message: Message):
+async def verify_stats_command(client: Bot, message: Message):
     verified_users_count = await count_verified_users()
     await message.reply(
         f"Total number of users who have verified their tokens: {verified_users_count}",
