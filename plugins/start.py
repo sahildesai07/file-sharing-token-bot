c03c12563fc11ead8d90c83fe65f0f9a91b2a6cd
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-08-30 23:23:05 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -152,7 +152,7 @@ async def start_command(client: Client, message: Message):
                 btn = [
                     [InlineKeyboardButton("Click here to Continue", url=link)],
                     [InlineKeyboardButton('Verification Tutorial', url=TUT_VID)],
-                    [InlineKeyboardButtom(text = 'Try Again', url = f"https://t.me/{client.username}?start={message.command[1]}")]
+                    #[InlineKeyboardButtom(text = 'Try Again', url = f"https://t.me/{client.username}?start={message.command[1]}")]
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
 
